34541696637d37aaad1cf546fa02e6f2bf021837
AdamBeresnev <adam.beresnev@gmail.com>
2025-12-01 01:16:29 +0200

    changed entry creation

--- internal/service/entry_service.go
+++ internal/service/entry_service.go
@@ -11,48 +11,60 @@ import (
 	"github.com/jmoiron/sqlx"
 )
 
-type EntryGeneration struct {
+type EntryService struct {
 	db    *sqlx.DB
 	store *store.TournamentStore
 }
 
-func (s EntryGeneration) ParseInput(ctx context.Context, entryLinks string) ([]bracket.Entry, []string, error) {
+func NewEntryService(db *sqlx.DB, store *store.TournamentStore) *EntryService {
+	return &EntryService{db: db, store: store}
+}
+
+func (s *EntryService) ParseInput(ctx context.Context, tournamentID string, entryLinks string) ([]bracket.Entry, error) {
 	var links []string
-	var returnLinks []string
 	var entries []bracket.Entry
-	var tounamentID uuid.UUID //TODO
 
 	links = strings.Split(entryLinks, "\n")
 
 	tx, err := s.db.BeginTxx(ctx, nil)
 	if err != nil {
-		return nil, links, err
+		return nil, err
 	}
 	defer tx.Rollback()
 
 	for i, link := range links {
-		e, err := createEntryFromLink(tounamentID, i, link)
+		e, err := createEntryFromLink(tournamentID, i, link)
 
 		if err != nil {
-			returnLinks = append(returnLinks, link)
 			continue
 		}
 
 		entries = append(entries, e)
 	}
 
-	return entries, returnLinks, tx.Commit()
+	err = s.store.CreateEntries(ctx, tx, entries)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return entries, tx.Commit()
 }
 
-func createEntryFromLink(tournamentID uuid.UUID, seed int, entryLink string) (bracket.Entry, error) {
-	var err error
+func createEntryFromLink(tournamentID string, seed int, entryLink string) (bracket.Entry, error) {
+	var entry bracket.Entry
 
 	placeholderName := "Anime"
 	placeholderImage := "https://pub-92474f7785774e91a790e086dfa6b2ef.r2.dev/anime/large-cover/PRdGioMCwXIr2ixbBXDC5yh4A70KA7vZ1JtqCJoE.jpg"
 
-	entry := bracket.Entry{
+	tournamentUUID, err := uuid.Parse(tournamentID) 
+	if err != nil {
+		return entry, err
+	}
+
+	entry = bracket.Entry{
 		ID:           uuid.New(),
-		TournamentID: tournamentID,
+		TournamentID: tournamentUUID,
 		Name:         placeholderName,
 		ImageLink:    &placeholderImage,
 		Seed:         seed,
